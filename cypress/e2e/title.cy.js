1ccb0433d044db6081edcda2dd880c044337f415
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-10-30 16:32:21 -0400

    fix: pass the body html if using parent command (#20)

--- cypress/e2e/title.cy.js
+++ cypress/e2e/title.cy.js
@@ -2,7 +2,7 @@
 
 it(
   'supports parent command',
-  { baseUrl: 'https://glebbahmutov.com' },
+  { baseUrl: 'https://glebbahmutov.com', scrollBehavior: 'center' },
   () => {
     cy.visit('/cypress-examples/')
     // normal command
@@ -10,6 +10,11 @@ it(
 
     cy.think(`
       the page title should include "Cypress Examples"
+    `)
+
+    cy.log('**Parent command with HTML**')
+    cy.think(`
+      in the main area find the anchor element with the text "Navigation" and scroll to it
     `)
   },
 )
